6ca979830ba459b1995e6deee190f46818fd1425
alisahin <alisgfx@gmail.com>
2014-10-28 00:24:46 +0100

    added less, added app.js, added main articles

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+// Self invoking function
+
+(function(){
+
+	// Modernizr
+	 if (Modernizr.touch) {   
+		console.log('Touch Screen');  
+	} else {   
+		console.log('No Touch Screen');  
+	} 
+})(); 
