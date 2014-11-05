a52626f46f6ad861813e521bd146e1c86a32d6ae
Ali <alisgfx@gmail.com>
2014-11-05 02:34:32 +0100

    minified app.js, fixed togglenav bug

---
+++ static/js/app-min.js
@@ -0,0 +1,1 @@
+!function(){Modernizr.touch?(document.querySelector("body > nav").classList.add("active"),console.log("Touch Screen")):(document.querySelector("body > nav").classList.add("hidden"),console.log("No Touch Screen"))}();
--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,16 @@
 // Self invoking function
-
 (function(){
 
 	// Modernizr
-	if (Modernizr.touch) {   
+	if (Modernizr.touch) {
+
+		// if touchScreen, add class active
+		document.querySelector('body > nav').classList.add('active');
 		console.log('Touch Screen');
-		document.querySelector('body > nav').classList.add('show');  
-	} else {   
+	} else {
+		
+		// if not touchScreen, add class hidden
+		document.querySelector('body > nav').classList.add('hidden');
 		console.log('No Touch Screen');
-		document.querySelector('body > nav').classList.add('hide');  
 	} 
 })(); 
